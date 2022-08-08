85118ebfb729487fd996ef4d794179946900d30c
unknown <bazo20061996@gmail.com>
2022-08-08 17:30:49 +0700

    up1

---
+++ src/Solution.java
@@ -0,0 +1,64 @@
+public class Solution {
+   static class Node{
+       int data;
+       Node linl;
+   }
+   static class Queue{
+      Node front,rear;
+   }
+   static void enQueue(Queue q,int value){
+      Node temp = new Node();
+      temp.data = value;
+      if (q.front == null){
+         q.front = temp;
+      }else {
+         q.rear.linl = temp;
+      }
+      q.rear =temp;
+      q.rear.linl = q.front;
+   }
+
+   static int deQueue(Queue q){
+      if (q.front == null){
+         System.out.printf(" Queue is empty");
+         return Integer.MIN_VALUE;
+      }
+      int value;
+      if (q.front == q.rear){
+         value = q.front.data;
+         q.front = null;
+         q.rear = null;
+      }else {
+         Node temp = q.front;
+         value = temp.data;
+         q.front = q.front.linl;
+         q.rear.linl = q.front;
+      }
+      return value;
+   }
+   static void disPlayQueue(Queue q){
+      Node temp = q.front;
+      System.out.printf(" Elements in Circular Queue are: ");
+      while (temp.linl != q.front){
+         System.out.printf("%d",temp.data);
+         temp = temp.linl;
+      }
+      System.out.printf("%d",temp.data);
+   }
+
+   public static void main(String[] args) {
+      Queue q = new Queue(); new Queue();
+      q.front = q.rear = null;
+      enQueue(q,14);
+      enQueue(q,22);
+      enQueue(q,6);
+      disPlayQueue(q);
+      System.out.printf(" Deleted value = %d",deQueue(q));
+      System.out.printf("Deleted value = %d",deQueue(q));
+      disPlayQueue(q);
+      enQueue(q,9);
+      enQueue(q,20);
+      disPlayQueue(q);
+   }
+
+}
